3436626cf6d1c58078875e3d7052c854402a0922
junior <101415099+rghmf@users.noreply.github.com>
2022-04-15 08:53:57 -0300

    Add files via upload

---
+++ pacman.py
@@ -0,0 +1,33 @@
+import pygame
+AMARELO = (255, 255, 0)
+PRETO = (0, 0, 0)
+VELOCIDADE = 1
+pygame.init()
+tela = pygame.display.set_mode((640, 480), 0)
+x = 10
+y = 10
+vel_x = VELOCIDADE
+vel_y = VELOCIDADE
+RAIO = 30
+while True:
+    #Calcula as regras
+    x = x + vel_x
+    y = y + vel_y
+    if x + RAIO > 640:
+        vel_x = -VELOCIDADE
+    if x - RAIO < 0:
+        vel_x = VELOCIDADE
+    if y + RAIO > 480:
+        vel_y = -VELOCIDADE
+    if y - RAIO < 0:
+        vel_y = VELOCIDADE
+    #Pinta
+    tela.fill(PRETO)
+    pygame.draw.circle(tela, AMARELO, (int(x), int(y)), 30, 0)
+    pygame.display.update()
+    #Eventos
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            exit()
+
+
